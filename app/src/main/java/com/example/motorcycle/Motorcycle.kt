7a0294df7222964af0e857dd49e75afeacf6d3df
ugureren00 <erenugur003@gmail.com>
2024-07-01 12:15:13 +0300

    ilk gönderim

---
+++ app/src/main/java/com/example/motorcycle/Motorcycle.kt
@@ -0,0 +1,10 @@
+package com.example.motorcycle
+
+abstract class Motorcycle(
+    val brand: String,
+    val model: String,
+    val year: Int,
+    val engineSize: Int
+) {
+    abstract fun printInfo()
+}
